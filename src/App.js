0958479f78c3997f0db0bbaa25e7a03d34923dcd
mushikhan1102 <mushikhan1102@gmail.com>
2024-03-20 13:56:17 -0400

    navbar, auth

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
+import React, {Fragment, useContext} from "react";
+import { BrowserRouter as Router, Routes, Route, Link, Navigate, Outlet } from 'react-router-dom'
+import Navbar from "./components/Navbar"
+import ReactDOM from 'react-dom';
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import AuthProvider from "./context/auth";
+import Login from "./pages/Login";
+import Profile from './pages/Profile'
+//import AuthProvider from "./context/auth";
+import PrivateRoute from "./components/PrivateRoute";
 import './App.css';
 
 function App() {
   return (
-    <div>Home</div>
-  );
+    <AuthProvider>
+    <Router>
+
+          <Navbar/>
+      <Routes>
+            <Route exact path='/register' element={< Register />}></Route>
+            <Route exact path='/Login' element={< Login />}></Route>
+
+            <Route element={<PrivateRoute />}>
+                {
+                    //<Route exact path='/Home' element={< Home />}></Route>
+                }
+                
+            </Route>
+            <Route exact path='/Profile' element={< Profile />}></Route>
+            <Route exact path='/Home' element={< Home />}></Route>
+            
+            
+           
+
+           
+        </Routes>
+
+    </Router>
+    </AuthProvider>
+
+);
 }
 
 export default App;

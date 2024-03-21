2a29cc9ddfaae7933c77331a1a0fe78ef13ceca1
chrishabre <habrecraft@hotmail.com>
2024-03-21 17:30:49 -0400

    changes to packages, app, firebase.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import Signup from "./pages/signup";
 
 
 function App() {
   return (
-    <div>testing</div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/signup" component={Signup} />
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 

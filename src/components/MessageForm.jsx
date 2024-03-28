c863d9a65e0d6f8f8799e52e80898f8efe7c4641
mushikhan1102 <mushikhan1102@gmail.com>
2024-03-28 18:29:38 -0400

    done with mile stone 2

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,30 +1,40 @@
 import React from "react";
-import Attachment from "./images/attach";
+import Attach from "../components/images/attach";
 
 const MessageForm = ({ handleSubmit, text, setText, setImg }) => {
   return (
     <form className="message_form" onSubmit={handleSubmit}>
-      <label htmlFor="img">
-        <Attachment />
-      </label>
-      <input
+     
+      {
+        <label htmlFor="img">
+          <Attach />
+        
+        </label>
+      }
+
+        <input
         onChange={(e) => setImg(e.target.files[0])}
         type="file"
         id="img"
         accept="image/*"
         style={{ display: "none" }}
-      />
+        />
+
       <div>
+        
         <input
           type="text"
           placeholder="Enter message"
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
+    
       </div>
+    
       <div>
         <button className="btn">Send</button>
       </div>
+    
     </form>
   );
 };

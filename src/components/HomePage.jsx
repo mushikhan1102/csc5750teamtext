29c09924f6477ab3676d859f8b8bd6da1e967d94
chrishabre <habrecraft@hotmail.com>
2024-04-18 18:07:14 -0400

    changes to home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -7,9 +7,9 @@ const HomePage = () => {
         <h1>About the Website</h1>
       </header>
       <main>
-        <p>Welcome to Team Text. This website is made to message anyone that registers to it. Click on Register ont eh top right to get started. Fill in the information there. 
+        <p>Welcome to Team Text. This website is made to message anyone that registers to it. Click on Register on the top right to get started. Fill in the information there. 
           Once you have done that, you will be sent back to the home page with a new option on the navbar called Chat. This leads to our messaging dashboard where you can see everyone that has logged into our website.
-          Our messaging system allows attachments such as images and GIFs or even full files. You can also customize your profile with a profile picture and a bio for more personalization.   </p>
+          Our messaging system allows attachments such as images and GIFs or even full files. You can also customize your profile with a profile picture for more personalization.   </p>
         <img src="https://via.placeholder.com/300" alt="Placeholder" />
       </main>
     </div>

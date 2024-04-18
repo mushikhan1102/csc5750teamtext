628c314d7394c6610524cd7d384d26b4de74e7d5
chrishabre <habrecraft@hotmail.com>
2024-04-18 17:48:16 -0400

    home page text

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -7,7 +7,8 @@ const HomePage = () => {
         <h1>About the Website</h1>
       </header>
       <main>
-        <p>Welcome to my simple React home page! This is a basic example of a React project.</p>
+        <p>Welcome to Team Text. This website is made to message anyone that registers to it. Click on Register ont eh top right to get started. Fill in the information there. Once you have done that, you will be sent to the login page. Fill in your login credentials you made and you will be sent to our main dashboard, where you can message anyone that has registered and logged in to the website. 
+          Our messaging system allows attachments such as images and GIFs or even full files. You can also customize your profile with a profile picture and a bio for more personalization.   </p>
         <img src="https://via.placeholder.com/300" alt="Placeholder" />
       </main>
     </div>
